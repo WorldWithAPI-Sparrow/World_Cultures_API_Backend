3bebc638539d5e1c7e09fa2d34f97c1394709287
AGROYGBV <amandagracerenfro@gmail.com>
2022-04-08 12:44:23 -0500

    added some seed data

--- seed.js
+++ seed.js
@@ -4,14 +4,14 @@ const fs = require("fs").promises;
 
 const { sequelize } = require("./db");
 const {
-  Continents,
-  Countries,
+  Continent,
+  Country,
   User,
-  TraditionalFoods,
-  Musics,
-  TouristAttractions,
-  Languages,
-  Currencies,
+  TraditionalFood,
+  Music,
+  TouristAttraction,
+  Language,
+  Currency,
 } = require("./models");
 
 const createUsers = async () => {
@@ -26,9 +26,60 @@ const createUsers = async () => {
   return users;
 };
 
-// const continents = [
-//     {continentName: "Africa", continentMap: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP_EKjSJ2y8CANXjipl8arGgTCRBqJi63VAQ&usqp=CAU"}
-// ];
+const createContinents = async () => {
+  const continents = [
+    {continentName: "Africa", 
+     continentMap: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTP_EKjSJ2y8CANXjipl8arGgTCRBqJi63VAQ&usqp=CAU"}
+];
+  return continents;
+};
+
+const createCountries = async () => {
+  const countries = [
+    {countryName: "Ethiopia", 
+     countryMap: "https://www.freeworldmaps.net/africa/ethiopia/ethiopia-physical-map.jpg",
+     ContinentId: 1}
+];
+  return countries;
+};
+
+const TraditionalFoods = async () => {
+  const traditionalFoods = [
+    {traditionalFoodName: "Injera", 
+     CountryId: 1},
+     {traditionalFoodName: "Gomen", 
+     CountryId: 1},
+     {traditionalFoodName: "Shiro", 
+     CountryId: 1},
+     {traditionalFoodName: "Sambusa", 
+     CountryId: 1},
+     {traditionalFoodName: "Kitfo", 
+     CountryId: 1}
+];
+  return traditionalFoods;
+};
+
+const Music = async () => {
+  const music = [
+    {songName: "Mar eske Tuwaf (Fikir Eske Meqabir)",
+     artistName: "Teddy Afro",
+     musicVideo: "https://www.youtube.com/watch?v=mFzHpK7ibfo", 
+     CountryId: 1}
+];
+  return music;
+};
+
+const TouristAttractions = async () => {
+  const touristAttractions = [
+    {placesToVisit: "Axum",
+     CountryId: 1},
+     {placesToVisit: "Lalibela",
+     CountryId: 1},
+     {placesToVisit: "The National Museum of Ethiopia",
+     CountryId: 1}
+];
+  return touristAttractions;
+};
 
 // function createCountryArray(results) {
 //   let countryResults = [];
@@ -44,9 +95,18 @@ const createUsers = async () => {
 const seed = async () => {
   await sequelize.sync({ force: true });
   const users = await createUsers();
+  const continents = await createContinents();
+  const countries = await createCountries();
+  const traditionalFoods = await createTraditionalFoods();
+  const music = await createMusic();
+  const touristAttractions = await createTouristAttraction();
+  const traditionalFoodPromises = traditionalFoods.map((traditionalFood) => TraditionalFoods.create(traditionalFood));
   const userPromises = users.map((user) => User.create(user));
-  //   const continentsPromises = continents.map((item) => Continents.create(continent));
-  await Promise.all([...userPromises]);
+  const continentPromises = continents.map((continent) => Continents.create(continent));
+  const countryPromises = countries.map((country) => Countries.create(country));
+  const musicPromises = music.map((music) => Music.create(music));
+  const touristAttractionPromises = touristAttractions.map((touristAttraction) => TouristAttractions.create(touristAttraction));
+  await Promise.all([...userPromises, ...continentPromises, ...countryPromises, ...traditionalFoodPromises, ...musicPromises, ...touristAttractionPromises]);
   console.log("database populated!");
 };
 
