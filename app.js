804d46d4604e8b9fcd1b307467b7b2b46d060dab
Diba <dibaja@ad.vzwcorp.com>
2022-04-15 10:44:07 -0500

    server and package.jason updated

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const routes = require("./routes/wcRoutes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = 3000;
 
 app.use(express.json());
--- routes/wcRoutes.js
+++ routes/wcRoutes.js
@@ -284,23 +284,47 @@ const routes = (app) => {
     res.json({ users });
   });
 
+
   // read one user by id
   app.get("/users/:id", jwtCheck, async (req, res) => {
     let user = await User.findByPk(req.params.id);
     res.json({ user });
   });
 
-  // create user
-  app.post("/users", async (req, res) => {
-    const name = req.body.name;
-    const password = req.body.password;
 
+  app.post('/login', async (req,res) =>{
+    const singleUser = await User.findOne({
+      where: {
+        userName: req.body.name,         
+        }
+    })      
+    console.log(req.params)
+    if(!signleUser) {
+      res.send('user not found')
+    } else {
+      bcrypt.compare(req.body.password, singleUser.password, async function(err, result){
+        if(result){
+          res.json(singleUser)
+        }else {
+          res.send("Passwords don't match")
+        }
+      })
+    }
+  });
+    
+    
+  
+  app.post("/signup", async (req, res) => {
+    const name = req.body.name
+    const password = req.body.password
+  
     bcrypt.hash(password, saltRounds, async function (err, hash) {
       const newUser = await User.create({ userName: name, userPassword: hash });
       console.log(hash);
       res.json({ newUser });
+      });
+       
     });
-  });
 
   //---------------------Routes ---------------------------------------
 
